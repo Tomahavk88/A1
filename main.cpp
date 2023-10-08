08184834b06ba88af0939ef37ccc5efbd2b7d8fc
Anton <ant.anshukoff2014@yandex.ru>
2023-10-08 18:32:51 +0300

    My name

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@ int main()
 	std::cout << "Hello word" << std ::endl;
 	std::cout << "Second line" << std :: endl;
 	std:: cout << "Third line" << std:: endl;
+	std:: cout << "Antonio" << std:: endl;
 	return 0;
 
 }	
